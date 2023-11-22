8c0a38d98c8b400f595dc5f6c7e719d5a007b25d
cincsobrepena <sobrepenacarliannoel@gmail.com>
2023-11-22 13:39:56 +0800

    Day 1 - Enabled Image and Name Query.

---
+++ script.js
@@ -0,0 +1,24 @@
+let searchName = document.querySelector("#searchBar");addEventListener("click", getPokemon);
+
+function getPokemon(e) {
+    const name = searchName.value;
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+        .then((response) => response.json())
+        .then((data) => {
+            document.querySelector(".entityCard").innerHTML = ` 
+            <h1 id="PokemonName" style=" margin 20px;">${data.name.toUpperCase()} </h1>
+            <img 
+                class="pokemonIMG"
+                src="${data.sprites.other["home"].front_default}"
+                alt="${data.name}"
+                style="width: 300px; margin 20px; filter: drop-shadow(30px 30px 20px rgba(70, 70, 70, 0.3))"
+            />
+            `;
+        })
+        .catch((err) => {
+            console.log("Pokemon not fount", err);
+        })
+
+        e.preventDefault();
+}
